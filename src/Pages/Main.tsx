19d0d43e751041700a6f2fd8e85deccc044ce6c9
Gal <galst1234@gmail.com>
2020-06-14 14:31:23 +0300

    Add basic map.

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,7 +1,13 @@
+// @ts-ignore
+import {ReactBingmaps} from 'react-bingmaps';
 import React from "react";
 
 export function Main() {
     return (
-        <span>Hello world</span>
-    )
+        <div style={{width: '720px', height: '720px'}}>
+            <ReactBingmaps
+                bingmapKey="Ar35sygI8qL7GUw_9hJk0fmUlmor4UTApHNA2Uq3kBrNNWJrDf2-XZ69FB6XKdub">
+            </ReactBingmaps>
+        </div>
+    );
 }
